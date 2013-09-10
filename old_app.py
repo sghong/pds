72ccf4d512336777a4dede5834d603ae79e8e9aa
Mr.Hong <sghong@gmail.com>
2013-09-10 16:39:22 +0900

    first thing in the morning

--- old_app.py
+++ old_app.py
@@ -4,4 +4,8 @@ Af   [ yes i am  ]ter waking up, what do you do?
 
 
 
-sir, it's traditional Korean mask, We call it 'taal'
+sir, it's tradition
+
+I go to the bathroom first thing in the morning.
+
+al Korean mask, We call it 'taal'
