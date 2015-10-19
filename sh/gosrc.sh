8f8ed912990eeb7b6ab7d1189fd3b83a3f8bea8b
Mr.Hong <sghong@gmail.com>
2015-10-19 17:14:22 +0900

    make cscope by dirlist.txt file

--- sh/gosrc.sh
+++ sh/gosrc.sh
@@ -1,57 +1,39 @@
 #!/bin/sh
 
+CURDIR=`pwd`
+
+if [ $# -ne 1 ]
+then
+    echo "Usage : gosrc.sh dir [dirlist.txt]"
+    exit
+fi
+
+if [ ! -d $1 ]
+then
+  mkdir $1
+fi
+
+CSCOPEFILENAME=$CURDIR/$1/dirlist.txt
+
+cd $1
+
 rm -rf cscope.files cscope.files cscope.out ncscope.out
 
+if [ ! -f $CSCOPEFILENAME ]
+then
+    echo "$CSCOPEFILENAME file not exist "
+    exit
+fi
 
 
-
-
-
-
-
-
-
-
-find $PWD/hardware/qcom/sensors ! \( -type d \) ! \( -type d -path '*.svn' -prune \) \
+for word in $( cat $CSCOPEFILENAME )
+do 
+  echo "===>$CURDIR/$word"
+  find $CURDIR/$word ! \( -type d \) ! \( -type d -path '*.svn' -prune \) \
        \( -name '*.[cChHsS]' -o -name '*.cpp' -o -name 'Makefile*' -o \
       -name 'makefile*' -o -name '*.cc' -o -name '*.mk' -o -name '*.sh' -o -name 'Kconfig' \
       -o -name 'KConfig' -o -name '*.mak' -o -name '*.min' -o -name '*.cmm' -o -name '*.txt' -o -name '*Sconscript*' -o -name '*scons' \
       -o -name '*config' -o -name '*.rc' -o -name '*.ld' -o -name '*.java' -o -name '*.cmd' -o -name '*.pm' -o -name '*.py' \
       -o -name '*.pl' -o -name '*.pds' -o -name '*.cfg' -o -name '*.scl' -o -name '*.xml' -o -name '*SConscript' -o -name '*SConstruct' \) -print >> cscope.files
-
-find $PWD/hardware/libhardware ! \( -type d \) ! \( -type d -path '*.svn' -prune \) \
-       \( -name '*.[cChHsS]' -o -name '*.cpp' -o -name 'Makefile*' -o \
-      -name 'makefile*' -o -name '*.cc' -o -name '*.mk' -o -name '*.sh' -o -name 'Kconfig' \
-      -o -name 'KConfig' -o -name '*.mak' -o -name '*.min' -o -name '*.cmm' -o -name '*.txt' -o -name '*Sconscript*' -o -name '*scons' \
-      -o -name '*config' -o -name '*.rc' -o -name '*.ld' -o -name '*.java' -o -name '*.cmd' -o -name '*.pm' -o -name '*.py' \
-      -o -name '*.pl' -o -name '*.pds' -o -name '*.cfg' -o -name '*.scl' -o -name '*.xml' -o -name '*SConscript' -o -name '*SConstruct' \) -print >> cscope.files
-
-find $PWD/kernel/drivers/staging/iio ! \( -type d \) ! \( -type d -path '*.svn' -prune \) \
-       \( -name '*.[cChHsS]' -o -name '*.cpp' -o -name 'Makefile*' -o \
-      -name 'makefile*' -o -name '*.cc' -o -name '*.mk' -o -name '*.sh' -o -name 'Kconfig' \
-      -o -name 'KConfig' -o -name '*.mak' -o -name '*.min' -o -name '*.cmm' -o -name '*.txt' -o -name '*Sconscript*' -o -name '*scons' \
-      -o -name '*config' -o -name '*.rc' -o -name '*.ld' -o -name '*.java' -o -name '*.cmd' -o -name '*.pm' -o -name '*.py' \
-      -o -name '*.pl' -o -name '*.pds' -o -name '*.cfg' -o -name '*.scl' -o -name '*.xml' -o -name '*SConscript' -o -name '*SConstruct' \) -print >> cscope.files
-      
-find $PWD/kernel/drivers/input/misc ! \( -type d \) ! \( -type d -path '*.svn' -prune \) \
-       \( -name '*.[cChHsS]' -o -name '*.cpp' -o -name 'Makefile*' -o \
-      -name 'makefile*' -o -name '*.cc' -o -name '*.mk' -o -name '*.sh' -o -name 'Kconfig' \
-      -o -name 'KConfig' -o -name '*.mak' -o -name '*.min' -o -name '*.cmm' -o -name '*.txt' -o -name '*Sconscript*' -o -name '*scons' \
-      -o -name '*config' -o -name '*.rc' -o -name '*.ld' -o -name '*.java' -o -name '*.cmd' -o -name '*.pm' -o -name '*.py' \
-      -o -name '*.pl' -o -name '*.pds' -o -name '*.cfg' -o -name '*.scl' -o -name '*.xml' -o -name '*SConscript' -o -name '*SConstruct' \) -print >> cscope.files
-
-find $PWD/frameworks/base/core/java/android/hardware/ ! \( -type d \) ! \( -type d -path '*.svn' -prune \) \
-       \( -name '*.[cChHsS]' -o -name '*.cpp' -o -name 'Makefile*' -o \
-      -name 'makefile*' -o -name '*.cc' -o -name '*.mk' -o -name '*.sh' -o -name 'Kconfig' \
-      -o -name 'KConfig' -o -name '*.mak' -o -name '*.min' -o -name '*.cmm' -o -name '*.txt' -o -name '*Sconscript*' -o -name '*scons' \
-      -o -name '*config' -o -name '*.rc' -o -name '*.ld' -o -name '*.java' -o -name '*.cmd' -o -name '*.pm' -o -name '*.py' \
-      -o -name '*.pl' -o -name '*.pds' -o -name '*.cfg' -o -name '*.scl' -o -name '*.xml' -o -name '*SConscript' -o -name '*SConstruct' \) -print >> cscope.files
-
-find $PWD/hardware/ ! \( -type d \) ! \( -type d -path '*.svn' -prune \) \
-       \( -name '*.[cChHsS]' -o -name '*.cpp' -o -name 'Makefile*' -o \
-      -name 'makefile*' -o -name '*.cc' -o -name '*.mk' -o -name '*.sh' -o -name 'Kconfig' \
-      -o -name 'KConfig' -o -name '*.mak' -o -name '*.min' -o -name '*.cmm' -o -name '*.txt' -o -name '*Sconscript*' -o -name '*scons' \
-      -o -name '*config' -o -name '*.rc' -o -name '*.ld' -o -name '*.java' -o -name '*.cmd' -o -name '*.pm' -o -name '*.py' \
-      -o -name '*.pl' -o -name '*.pds' -o -name '*.cfg' -o -name '*.scl' -o -name '*.xml' -o -name '*SConscript' -o -name '*SConstruct' \) -print >> cscope.files
+done
 cscope -b -k -i cscope.files
-
